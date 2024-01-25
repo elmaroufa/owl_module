ac51600036831d5f95db768fabd189b7d6bebdaa
elmaroufa <salyabbo@gmail.com>
2024-01-25 17:11:32 +0100

    feature add information other kpi content(order,revenus,average)

--- owl/static/src/components/dashboard/sales_dashboard.js
+++ owl/static/src/components/dashboard/sales_dashboard.js
@@ -18,15 +18,17 @@ export class OwlSalesDashboard extends Component {
             
         })
         this.orm = useService("orm");
+        this.actionService = useService("action");
         onWillStart(async () => {
             await this.getQuotations();
-           
+            await this.getOrders();
         });
     }
 
     async onChangePeriod() {
         this.getDates();
         await this.getQuotations();
+        await this.getOrders();
     }
 
     getDates(){
@@ -50,10 +52,53 @@ export class OwlSalesDashboard extends Component {
         }
         const prev_data = await this.orm.searchCount("sale.order",previous_domain);
         const percentage = ((data - prev_data)/prev_data) * 100 ;
-        this.state.quotations.percentage = percentage;
+        this.state.quotations.percentage = percentage.toFixed(2);
         console.log(this.state.previous_date, this.state.current_date);
     }
 
+    async getOrders(){
+        let domain = [['state','in',['sale','done']]];
+        if (this.state.period > 0){
+            domain.push(['date_order','>',this.state.current_date]);
+            console.log(this.state.previous_date);
+        }
+        const data = await this.orm.searchCount("sale.order",domain);
+        //this.state.quotations.value = data;
+
+        //previous_date
+        let previous_domain = [['state','in',['sale','done']]];
+        if (this.state.period > 0){
+            previous_domain.push(['date_order','>',this.state.previous_date],['date_order','<=',this.state.current_date]);
+        }
+        const prev_data = await this.orm.searchCount("sale.order",previous_domain);
+        const percentage = ((data - prev_data)/prev_data) * 100 ;
+        //this.state.quotations.percentage = percentage.toFixed(2);
+
+        // revenues
+        const current_revenue = await this.orm.readGroup("sale.order", domain, ["amount_total:sum"], []);
+        const previous_revenue = await this.orm.readGroup("sale.order", previous_domain, ["amount_total:sum"], []);
+        const revenue_percentage = ((current_revenue[0].amount_total - previous_revenue[0].amount_total)/
+        previous_revenue[0].amount_total) * 100
+
+        // average
+        const current_average = await this.orm.readGroup("sale.order", domain, ["amount_total:avg"], []);
+        const previous_average = await this.orm.readGroup("sale.order", previous_domain, ["amount_total:avg"], []);
+        const average_percentage = ((current_average[0].amount_total - previous_average[0].amount_total)/
+        previous_average[0].amount_total) * 100
+        
+
+        this.state.orders = {
+            value: data,
+            percentage: percentage.toFixed(2),
+            revenue: `${(current_revenue[0].amount_total/1000).toFixed(2)}K`,
+            revenue_percentage : revenue_percentage.toFixed(2),
+            average: `${(current_average[0].amount_total/1000).toFixed(2)}K`,
+            average_percentage : average_percentage.toFixed(2)
+        }
+    }
+    viewQuotations(){
+        this.actionService.doAction("sale.action_quotations_with_onboarding");
+    }
    
 }
 
